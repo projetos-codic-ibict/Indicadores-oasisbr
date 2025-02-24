bdffc7fdad39f02dd2c890339db05c490237118b
Jesiel Viana <jesielviana@proton.me>
2025-02-24 12:12:42 -0300

    corrigindo a forma de pegar a data atual

--- plots/indicadores_evolucao_plot.R
+++ plots/indicadores_evolucao_plot.R
@@ -1,8 +1,9 @@
 oasisbrEvolucao <- tryCatch(
   {
     # Formatar a data no formato "dia/mes/ano"
-    # data_formatada <- format(data_atual, "%d/%m/%Y")
-    oasisbrEvolucao <- fromJSON("http://172.16.17.19:3000/api/v1/evolution-indicators?init=10/10/2010&end=24/02/2025")
+    data_atual <- Sys.Date()
+    data_formatada <- format(data_atual, "%d/%m/%Y")
+    oasisbrEvolucao <- fromJSON("http://172.16.17.19:3000/api/v1/evolution-indicators?init=10/10/2010&end=", data_formatada)
     print("========================")
     print("Indicadores de evolução:")
     print("STATUS: ONLINE")
